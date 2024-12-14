47c3a25540fbf15a54b41ca1ccaa9d97b8132718
selvarahul <162546430+selvarahul@users.noreply.github.com>
2024-12-14 23:31:45 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Q1{
+   public static void main(String agrs[]){
+    int[] A=new int[5];
+    Scanner scan =new Scanner(System.in);
+    A[5]=scan.nextInt();
+    
+}
+}
